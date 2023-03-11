c5f6e474c00eb2a92f876047813caf0a13402707
arch-rengo <rengo@rengo.dev>
2023-03-11 11:46:13 -0500

    rook returns

--- tools/hosted_clean.sh
+++ tools/hosted_clean.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-for PVC in $(kubectl get persistentvolumeclaims -n hosted | awk '/-lvm/ {print $1}'); do
+for PVC in $(kubectl get persistentvolumeclaims -n rook-ceph | awk '/-lvm/ {print $1}'); do
     echo PVC
-    kubectl get persistentvolumeclaim/"$PVC" -o name -n hosted | \
-    xargs -I {} kubectl patch {} -n hosted --type merge -p '{"metadata":{"finalizers": []}}'
+    kubectl get persistentvolumeclaim/"$PVC" -o name -n rook-ceph | \
+    xargs -I {} kubectl patch {} -n rook-ceph --type merge -p '{"metadata":{"finalizers": []}}'
 done

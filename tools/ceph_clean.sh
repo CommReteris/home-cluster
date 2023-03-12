3db85ddc8397ac06d03ddaab25cac4968c37af09
arch-rengo <rengo@rengo.dev>
2023-03-12 18:21:19 -0400

    ceph it again but skip dynamic local provisioning for OSDs

--- tools/ceph_clean.sh
+++ tools/ceph_clean.sh
@@ -3,10 +3,11 @@ for CRD in $(kubectl get crd -n rook-ceph | awk '/ceph.rook.io/ {print $1}'); do
     if [ $CRD != 'cephclusters.ceph.rook.io' ]; then
         kubectl get -n rook-ceph "$CRD" -o name | \
         xargs -I {} kubectl patch -n rook-ceph {} --type merge -p '{"metadata":{"finalizers": []}}'
+        kubectl get -n rook-ceph "$CRD" -o name | xargs -I {} kubectl delete -n rook-ceph {}
     else
         kubectl get -n rook-ceph "$CRD" -o name | \
         xargs -I {} kubectl patch -n rook-ceph {}  --type merge \
          -p '{"spec":{"cleanupPolicy":{"confirmation":"yes-really-destroy-data"}}}'
     fi
-    kubectl get -n rook-ceph "$CRD" -o name | xargs -I {} kubectl delete -n rook-ceph {}
 done
+kubectl get -n rook-ceph cephclusters.ceph.rook.io -o name | xargs -I {} kubectl delete -n rook-ceph {}

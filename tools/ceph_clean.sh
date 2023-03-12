4da35b934e7c9e9309b149d411c131b3ff6e34f4
arch-rengo <rengo@rengo.dev>
2023-03-12 17:23:46 -0400

    tweak ceph mons

--- tools/ceph_clean.sh
+++ tools/ceph_clean.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 for CRD in $(kubectl get crd -n rook-ceph | awk '/ceph.rook.io/ {print $1}'); do
-    kubectl get -n rook-ceph "$CRD" -o name | \
-    xargs -I {} kubectl patch -n rook-ceph {} --type merge -p '{"metadata":{"finalizers": []}}'
+    if [ $CRD != 'cephclusters.ceph.rook.io' ]; then
+        kubectl get -n rook-ceph "$CRD" -o name | \
+        xargs -I {} kubectl patch -n rook-ceph {} --type merge -p '{"metadata":{"finalizers": []}}'
+    else
+        kubectl get -n rook-ceph "$CRD" -o name | \
+        xargs -I {} kubectl patch -n rook-ceph {}  --type merge \
+         -p '{"spec":{"cleanupPolicy":{"confirmation":"yes-really-destroy-data"}}}'
+    fi
+    kubectl get -n rook-ceph "$CRD" -o name | xargs -I {} kubectl delete -n rook-ceph {}
 done
